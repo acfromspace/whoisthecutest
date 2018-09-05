9d0389dd2c13148a5e33957d593f59aca926bfa9
AC De Leon <aadeleon@sdsu.edu>
2018-09-05 15:52:39 -0700

    [*] Everything working correctly

--- src/pages/who.js
+++ src/pages/who.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Icon } from 'semantic-ui-react'
+import { Button, Icon, Segment } from 'semantic-ui-react'
 
 import Layout from '../components/layout'
 
@@ -8,11 +8,11 @@ const SecondPage = () => (
     <h2><span role="img" aria-label="Thinking Face">🤔</span> Who made this?</h2>
     <p>Hello there, I go by @acfromspace, the creator of this website.</p>
     <p>Goal of this website is to share with those in need of a smile.</p>
-    <p>From the National Institute of Mental Health in 2016, suicide was the second leading cause of death among
+    <Segment>From the National Institute of Mental Health in 2016, suicide was the second leading cause of death among
       individuals between the ages of 10 and 34, and the fourth leading cause of death among individuals between
       the ages of 35 and 54. Suicide was the tenth leading cause of death overall in the United States,
       claiming the lives of nearly 45,000 people. From years 1999 to 2016, the death rate from suicides dramatically
-      rose by 28%.</p>
+      rose by 28%.</Segment>
     <p>Mental health is a serious issue, and we all deserve a chance to be happy and knowing everything will be okay.</p>
     <p>Reach out to me if you'd like to!</p>
     <Button color='github' onClick={() => window.open("https://github.com/acfromspace", "_blank")}>
