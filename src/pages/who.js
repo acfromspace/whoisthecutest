9915f5b468a7ddcdef36c3eba1b6f1e8088d6d58
AC De Leon <aadeleon@sdsu.edu>
2018-09-04 22:00:01 -0700

    [+] react-player

--- src/pages/who.js
+++ src/pages/who.js
@@ -5,7 +5,9 @@ import Layout from '../components/layout'
 const SecondPage = () => (
   <Layout>
     <h2><span role="img" aria-label="Thinking Face">🤔</span> Who made this?</h2>
-    <p>I am @acfromspace!</p>
+    <p>Hello there, I go by @acfromspace, the creator of this website.</p>
+    <p>Goal of this website is to share with those in need of a smile.</p>
+    <p>Mental health is a serious issue, and we all deserve a chance to be happy and knowing everything will be okay.</p>
   </Layout>
 )
 
