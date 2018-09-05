5e75b1ae65d077d7bd676c8d0ef4f0bded7f0107
AC De Leon <aadeleon@sdsu.edu>
2018-09-04 23:43:15 -0700

    [*] Testing mobile import PropTypes

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
 
-const Header = ({ siteTitle }) => (
+const Header = ({ mobile, siteTitle }) => (
   <div style={{ background: 'linear-gradient(45deg, #ffd1dc, #b19cd9)' }}>
-      <h1 style={{ padding: '10rem', textAlign: 'center' }}>
-        <Link style={{ color: 'white', font: '50px Georgia' }} to="/">
+      <h1 style={{ padding: mobile ? '2rem' : '10rem', textAlign: 'center' }}>
+        <Link style={{ color: 'white', font: mobile ? '1em Georgia' : '3em Georgia' }} to="/">
           {siteTitle}
         </Link>
       </h1>
   </div>
 )
+
+Header.propTypes = {
+  mobile: PropTypes.bool,
+}
 
 export default Header
