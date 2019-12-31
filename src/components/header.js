c3a26b1a9be1138b588277a4fe7072b5a97f421b
AC De Leon <acfromspace.deleon@gmail.com>
2019-12-31 09:04:50 -0800

    [*] header.js 🌱

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types'
 const Header = ({ mobile, siteTitle }) => (
   <div style={{ background: 'linear-gradient(45deg, #ffd1dc, #b19cd9)' }}>
       <h1 style={{ padding: mobile ? '1rem' : '10rem', textAlign: 'center' }}>
-        <Link style={{ color: 'white', font: mobile ? '12px Georgia' : '3em Georgia' }} to="/">
+        <Link style={{ color: 'white', font: mobile ? '12px Georgia' : '3rem Georgia' }} to="/">
           {siteTitle}
         </Link>
       </h1>
